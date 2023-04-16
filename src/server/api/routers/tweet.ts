06693a481b4bf3a3194fcb40c6dcd6bcce813be8
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-04-16 11:02:31 +0200

    Cursor based pagination

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -29,8 +29,15 @@ export const tweetRouter = createTRPCRouter({
       const tweets = await prisma.tweet.findMany({
         take: limit + 1,
         orderBy: [{ createdAt: "desc" }],
+        cursor: cursor ? { id: cursor } : undefined,
         include: { author: { select: { name: true, image: true, id: true } } },
       });
-      return { tweets };
+      let nextCursor: typeof cursor | undefined = undefined;
+      if (tweets.length > limit) {
+        const nextItem = tweets.pop() as (typeof tweets)[number];
+        nextCursor = nextItem.id;
+      }
+
+      return { tweets, nextCursor };
     }),
 });
